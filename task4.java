0105099f8955627bba4315df66dbbe802f09404e
dngrvn <108631571+dngrvn@users.noreply.github.com>
2023-03-24 00:06:14 +0300

    Add files via upload

---
+++ task4.java
@@ -0,0 +1,15 @@
+package job;
+
+// (Не обязательная)
+//  На шахматной доске расставить 8 ферзей так, 
+// чтобы они не били друг друга.
+
+public class task4 {
+
+    public static void main(String[] args) {
+
+        
+        
+    }
+}
+
